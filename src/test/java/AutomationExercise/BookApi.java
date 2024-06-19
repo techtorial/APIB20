db31b3a0daf8c8716532ef39a60a9f93e7a0f663
techtorial <50930535+techtorial@users.noreply.github.com>
2024-06-18 20:59:45 -0500

    Latest Code

--- src/test/java/AutomationExercise/BookApi.java
+++ src/test/java/AutomationExercise/BookApi.java
@@ -14,14 +14,14 @@ public class BookApi {
     1-I want you validate the token is not null with RestAssured and Body
      */
     @Test
-    public void getToken(){
+    public void getToken() {
 
-        RestAssured.baseURI="https://restful-booker.herokuapp.com";
-        RestAssured.basePath="/auth";
+        RestAssured.baseURI = "https://restful-booker.herokuapp.com";
+        RestAssured.basePath = "/auth";
 
-        Response response= RestAssured.given()
-                .header("Content-Type","application/json")
-                .header("Accept","application/json")
+        Response response = RestAssured.given()
+                .header("Content-Type", "application/json")
+                .header("Accept", "application/json")
                 .body("{\n" +
                         "    \"username\" : \"admin\",\n" +
                         "    \"password\" : \"password123\"\n" +
@@ -30,13 +30,31 @@ public class BookApi {
                 .post()
                 .then().statusCode(200).log().body().extract().response();
 
-        Map<String,Object> deserializedToken=response.as(new TypeRef<Map<String, Object>>() {});
+        Map<String, Object> deserializedToken = response.as(new TypeRef<Map<String, Object>>() {
+        });
         Assertions.assertNotNull(deserializedToken.get("token"));
     }
 
+    @Test
+    public void validateBookDetails() {
 
+        RestAssured.baseURI = "https://restful-booker.herokuapp.com";
+        RestAssured.basePath = "/booking/99";
 
+        Response response = RestAssured.given().header("Accept", "application/json")
+                .when().get()
+                .then().statusCode(200).log().body().extract().response();
 
+        Map<String, Object> deserializedResponse = response.as(new TypeRef<Map<String, Object>>() {
+        });
 
+        Assertions.assertEquals("Josh", deserializedResponse.get("firstname"));
+        Assertions.assertEquals(true, deserializedResponse.get("depositpaid"));
+
+        Map<String,Object> bookingDatesInformation= (Map<String, Object>) deserializedResponse.get("bookingdates");
+        Assertions.assertEquals("2018-01-01",bookingDatesInformation.get("checkin"));
+    }
+
+        //Please do create and update automation. Update needs Authorization(Basic etsts34234) id can be AssertNotNull
 
 }
