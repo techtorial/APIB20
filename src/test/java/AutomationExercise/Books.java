99aa934182e1f13e31348cd72d7b4280cd5b0183
techtorial <50930535+techtorial@users.noreply.github.com>
2024-06-22 09:35:37 -0500

    Latest Code

--- src/test/java/AutomationExercise/Books.java
+++ src/test/java/AutomationExercise/Books.java
@@ -24,4 +24,45 @@ public class Books {
         Assertions.assertEquals("2018-01-01",deserializedResponse.getBookingdates().getCheckin());
 
     }
+
+    @Test
+    public void validateUpdateFunctionalityOfAPIWithPojo(){
+
+        RestAssured.baseURI="https://restful-booker.herokuapp.com";
+        RestAssured.basePath="/booking/83";
+
+        Response response=RestAssured.given().contentType("application/json").accept("application/json")
+                .header("Authorization","Basic YWRtaW46cGFzc3dvcmQxMjM=")
+                .body("{\n" +
+                        "    \"firstname\" : \"Ahmet\",\n" +
+                        "    \"lastname\" : \"Baldir\",\n" +
+                        "    \"totalprice\" : 555,\n" +
+                        "    \"depositpaid\" : true,\n" +
+                        "    \"bookingdates\" : {\n" +
+                        "        \"checkin\" : \"2025-01-01\",\n" +
+                        "        \"checkout\" : \"2030-01-01\"\n" +
+                        "    },\n" +
+                        "    \"additionalneeds\" : \"UPDATED SOFTWARE ENGINEER\"\n" +
+                        "}")
+                .when().put().then().statusCode(200).log().body().extract().response();
+
+    BookPojo deserializedResponse=response.as(BookPojo.class);
+
+     Assertions.assertEquals(555,deserializedResponse.getTotalprice());
+     Assertions.assertEquals("2030-01-01",deserializedResponse.getBookingdates().getCheckout());
+     Assertions.assertEquals("UPDATED SOFTWARE ENGINEER",deserializedResponse.getAdditionalneeds());
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 }
